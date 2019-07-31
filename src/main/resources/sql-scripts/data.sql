aaab013afae6297c16332d2953efdc365f5d4a55
sharon999 <shanoked@gmail.com>
2019-07-31 15:47:56 +0300

    Update data.sql
    change data 2

--- src/main/resources/sql-scripts/data.sql
+++ src/main/resources/sql-scripts/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (1,11, 53,'tr','bread',2);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (2,12, 88,'tr','milk',1);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (3,13, 77,'tr','water',3);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (4,14, 35,'tr','juice',4);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (5,15, 48,'tr','tomato',5);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (6,16 ,27,'tr','grape',7);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (7,17, 47,'tr','apple',6);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (8,18 ,31,'tr','egg',5);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (1, 53,'tr','bread',2);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (2, 88,'tr','milk',1);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (3, 77,'tr','water',3);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (4, 35,'tr','juice',4);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (5, 48,'tr','tomato',5);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (6,27,'tr','grape',7);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (7, 47,'tr','apple',6);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (8,31,'tr','egg',5);

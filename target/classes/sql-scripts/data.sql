f23ed9d387930d601f57c2f714229fe49eba9706
sharon999 <shanoked@gmail.com>
2019-07-31 15:46:52 +0300

    Update data.sql
    change data2

--- target/classes/sql-scripts/data.sql
+++ target/classes/sql-scripts/data.sql
@@ -1,18 +1,10 @@
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (1, '53','tr','Rize',2);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (2, '08','tr','Artvin',1);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (3, '07','tr','Antalya',3);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (4, '35','tr','İzmir',4);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (5, '48','tr','Muğla',5);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (6, '27','tr','GaziAntep',7);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (7, '47','tr','Mardin',6);
-INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (8, '31','tr','Hatay',5);
-/*
-INSERT INTO item(id,num, item_code,  inventory_code, item_name, quantity) VALUES (1,11, 53,'tr','bread',2);
-INSERT INTO item(id, num,item_code,  inventory_code, item_name, quantity) VALUES (2,12, 88,'tr','milk',1);
-INSERT INTO item(id,num, item_code,  inventory_code, item_name, quantity) VALUES (3,13, 77,'tr','water',3);
-INSERT INTO item(id, num,item_code,  inventory_code, item_name, quantity) VALUES (4,14, 35,'tr','juice',4);
-INSERT INTO item(id,num, item_code,  inventory_code, item_name, quantity) VALUES (5,15, 48,'tr','tomato',5);
-INSERT INTO item(id,num, item_code,  inventory_code, item_name, quantity) VALUES (6,16 ,27,'tr','grape',7);
-INSERT INTO item(id,num, item_code,  inventory_code, item_name, quantity) VALUES (7,17, 47,'tr','apple',6);
-INSERT INTO item(id,num, item_code,  inventory_code, item_name, quantity) VALUES (8,18 ,31,'tr','egg',5);
-*/
+
+INSERT INTO item(id,item_code,  inventory_code, item_name, quantity) VALUES (1, 53,'tr','bread',2);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (2, 88,'tr','milk',1);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (3, 77,'tr','water',3);
+INSERT INTO item(id,item_code,  inventory_code, item_name, quantity) VALUES (4, 35,'tr','juice',4);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (5, 48,'tr','tomato',5);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (6,27,'tr','grape',7);
+INSERT INTO item(id,item_code,  inventory_code, item_name, quantity) VALUES (7, 47,'tr','apple',6);
+INSERT INTO item(id, item_code,  inventory_code, item_name, quantity) VALUES (8,31,'tr','egg',5);
+
